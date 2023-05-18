e65e153af29fe8bc72a662cbdcc5c8824dea7f2e
Nikas Ghimire <wasdthedebugger@gmail.com>
2023-05-18 16:50:46 +0545

    Infinite Scroll

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,17 @@
 <script>
+  import { onMount, afterUpdate } from "svelte";
   import Feed from "./Feed.svelte";
   import Footer from "./Footer.svelte";
   import Nav from "./Nav.svelte";
   import Sidebar from "./Sidebar.svelte";
-  import { onMount } from "svelte";
 
   let feeds = [];
+  let loading = false;
+  let scrollContainer;
 
   async function fetchMemes() {
     try {
+      loading = true;
       let memesFetched = 0;
       while (memesFetched < 10) {
         const response = await fetch(
@@ -24,20 +27,42 @@
       }
     } catch (error) {
       console.error("Failed to fetch memes:", error);
+    } finally {
+      loading = false;
     }
   }
 
   onMount(fetchMemes);
+
+  function loadMore() {
+    fetchMemes();
+  }
+
+  afterUpdate(() => {
+    if (scrollContainer) {
+      // Scroll to the bottom of the feed only when new memes are added and not when updating
+      const { scrollTop, scrollHeight, clientHeight } = scrollContainer;
+      const atBottom = scrollTop + clientHeight === scrollHeight;
+      if (atBottom) {
+        scrollContainer.scrollTop = scrollContainer.scrollHeight;
+      }
+    }
+  });
 </script>
 
 <Nav />
 <br />
 <div class="main">
   <Sidebar />
-  <div class="scroll">
+  <div class="scroll" bind:this={scrollContainer}>
     {#each feeds as feed}
       <Feed imageUrl={feed.imageUrl} />
     {/each}
+    {#if loading}
+      <div class="loading">Loading</div>
+    {:else if feeds.length >= 10}
+      <button on:click={loadMore}>See More</button>
+    {/if}
   </div>
 </div>
 <br />
@@ -63,12 +88,37 @@
     background-color: #333;
   }
 
-  /* dont show scroll bar of scroll */
+  /* don't show scroll bar of scroll */
   .scroll::-webkit-scrollbar {
     display: none;
   }
 
-  /* show the sidebar at top in small screens*/
+  .scroll button {
+    width: 100%;
+    height: 100%;
+    border: none;
+    padding: 10px;
+    font-size: 1rem;
+    font-weight: 600;
+    cursor: pointer;
+    padding: 10px;
+  }
+
+  .loading{
+    width: 100%;
+    height: 50px;
+    border: none;
+    padding: 10px;
+    font-size: 1rem;
+    font-weight: 600;
+    cursor: pointer;
+    padding: 10px;
+    color: #fff;
+    text-align: center;
+    background-color: #aea7a7d1;
+  }
+
+  /* show the sidebar at top on small screens */
   @media screen and (max-width: 800px) {
     .main {
       flex-direction: column;
